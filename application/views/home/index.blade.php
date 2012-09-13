cc18b1df3000a8956359a28436ac177e2d8eb466
Graeme George <graemelgeorge@gmail.com>
2012-09-13 16:09:09 +0100

    Initial commit

---
+++ application/views/home/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Laravel: A Framework For Web Artisans</title>
+	<meta name="viewport" content="width=device-width">
+</head>
+<body>
+
+	{{ $greeting }}
+	
+</body>
+</html>
